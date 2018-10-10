af06e877e2e4646acd438493eb544f7212a200b7
Jakob Urnes <jdubs163@gmail.com>
2018-10-10 14:04:54 +0200

    project part 1

---
+++ scala_project/task1-project/src/main/scala/Account.scala
@@ -0,0 +1,27 @@
+    import exceptions._
+
+class Account(val bank: Bank, initialBalance: Double) {
+
+    class Balance(var amount: Double) {
+        val bal = amount;
+        def setBal(var entry: Double): Unit = {
+            bal = entry;
+        }
+        def getBal(): Double = {
+            return bal;
+        }
+    }
+
+    val balance = new Balance(initialBalance)
+    val uid = bank.generateAccountId
+
+    def withdraw(amount: Double): Unit = balance.setBal(balance.getBal-amount);
+    def deposit(amount: Double): Unit = balance.setBal(balance.getBal+amount);
+    def getBalanceAmount: Double = return balance.getBal
+
+    def transferTo(account: Account, amount: Double) = {
+        bank addTransactionToQueue (this, account, amount)
+    }
+
+
+}
