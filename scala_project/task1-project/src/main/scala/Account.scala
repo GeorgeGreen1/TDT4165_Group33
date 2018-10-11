569e27aa0175e308aac2109835b00031a61bbcbc
Savagerqt <tanguy.viv@gmail.com>
2018-10-11 17:30:53 +0200

    Test 1-6

--- scala_project/task1-project/src/main/scala/Account.scala
+++ scala_project/task1-project/src/main/scala/Account.scala
@@ -2,27 +2,67 @@
 
 class Account(val bank: Bank, initialBalance: Double) {
 
-    class Balance(var amount: Double) {
-        val bal = amount;
-        def setBal(var entry: Double): Unit = {
-            bal = entry;
-        }
-        def getBal(): Double = {
+    class Balance(amount : Double) {
+
+        // Current value of the Balance
+        private var bal = amount;
+
+        // Get value of Balance
+        def getBalance() : Double = {
             return bal;
-	    // test for git pushing
         }
+
+        // Add amount from current balance
+        def add(amount : Double) = {
+            this.synchronized{
+                bal = bal + amount;
+            }
+        }
+
+        // Substract from current balance
+        def substract(amount : Double) = {
+            this.synchronized{
+                bal = bal - amount;
+            }
+        }
+
+        // Return if the balance is superior to the asked
+        // amount
+        def hasSufficientFunds(amount : Double) : Boolean = {
+            this.synchronized{
+                return (bal > amount);
+            }
+        }
+
     }
 
     val balance = new Balance(initialBalance)
     val uid = bank.generateAccountId
 
-    def withdraw(amount: Double): Unit = balance.setBal(balance.getBal-amount);
-    def deposit(amount: Double): Unit = balance.setBal(balance.getBal+amount);
-    def getBalanceAmount: Double = return balance.getBal
-
-    def transferTo(account: Account, amount: Double) = {
-        bank addTransactionToQueue (this, account, amount)
+    def withdraw(amount: Double): Unit = {
+        if (amount < 0) {
+            throw new IllegalAmountException();
+        }
+        else if (balance.hasSufficientFunds(amount)) {
+            balance.substract(amount);
+        }
+        else {
+            throw new NoSufficientFundsException();
+        }
     }
 
+    def deposit(amount: Double): Unit = {
+        if (amount < 0) {
+            throw new IllegalAmountException();
+        }
+        balance.add(amount);
+    }
 
+    def getBalanceAmount: Double = {
+        return balance.getBalance();
+    }
+
+    def transferTo(account: Account, amount: Double) = {
+        bank addTransactionToQueue (this, account, amount);
+    }
 }
--- scala_project/task1-project/src/main/scala/Bank.scala
+++ scala_project/task1-project/src/main/scala/Bank.scala
@@ -5,17 +5,17 @@ class Bank(val allowedAttempts: Integer = 3) {
     private val uid = 0
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
-    private val executorContext = ???
+    private val executorContext = 0
 
     def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
       transactionsQueue push new Transaction(
         transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
     }
 
-    // Hint: use a counter 
-    def generateAccountId: Int = return (uid++;)
+    // Hint: use a counter
+    def generateAccountId: Int = {return 0;}
 
-    private def processTransactions: Unit = ???
+    private def processTransactions: Unit = {return}
 
     def addAccount(initialBalance: Double): Account = {
         new Account(this, initialBalance)
